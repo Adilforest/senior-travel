11b18046fbc7e96235df310b7e93233ef36b4378
Adil_Forest <ormanov.adik@gmail.com>
2024-11-19 08:45:10 +0500

    Adding adaptive.

--- js/theme-toggle.js
+++ js/theme-toggle.js
@@ -25,10 +25,11 @@ themeToggle.addEventListener('click', () => {
     }
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+    const menuToggle = document.getElementById("menu-toggle");
+    const navLinks = document.getElementById("nav-links");
 
-const menuToggle = document.getElementById('menu-toggle');
-const navLinks = document.querySelector('.nav-links');
-
-menuToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('show');
+    menuToggle.addEventListener("click", () => {
+        navLinks.classList.toggle("show");
+    });
 });
